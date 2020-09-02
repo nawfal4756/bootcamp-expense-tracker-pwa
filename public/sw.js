ad9b0f22bb81bead5c5b9af5f833e76a55d53f80
nawfal4756 <nawfal4756@users.noreply.github.com>
2020-09-02 15:11:42 +0500

    Service Worker Fixed

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,18 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [];
+var CACHE_NAME = 'expense-tracker-v1';
+var urlsToCache = [
+	'./static/js/bundle.js',
+	'./static/js/0.chunk.js',
+	'./static/js/main.chunk.js',
+	'./static/js/5.chunk.js',
+	'./static/js/2.chunk.js',
+	'./static/js/3.chunk.js',
+	'./static/js/1.chunk.js',
+	'./static/js/4.chunk.js',
+	'./manifest.json',
+	'./favicon2.ico',
+	'./',
+	'index.html'
+];
 
 self.addEventListener('install', function(event) {
 	// Perform install steps
@@ -8,26 +21,52 @@ self.addEventListener('install', function(event) {
 			.open(CACHE_NAME)
 			.then(function(cache) {
 				console.log('Opened cache');
-				return cache.addAll(arr(urlsToCache));
+				return cache.addAll(urlsToCache);
 			})
 			.catch((err) => console.log('err', err))
 	);
 });
 
-self.addEventListener('fetch', function(event) {
+// self.addEventListener('fetch', function(event) {
+// 	if (!navigator.onLine) {
+// 		event.respondWith(
+// 			caches
+// 				.match(event.request)
+// 				.then(function(response) {
+// 					// Cache hit - return response
+// 					if (response) {
+// 						return response;
+// 					}
+// 					let requestUrl = event.request.clone();
+// 					return fetch(requestUrl);
+// 				})
+// 				.catch((err) => console.log('err', err))
+// 		);
+// 	}
+// });
+
+addEventListener('fetch', function(event) {
 	if (!navigator.onLine) {
 		event.respondWith(
-			caches
-				.match(event.request)
-				.then(function(response) {
-					// Cache hit - return response
-					if (response) {
-						return response;
-					}
-					let requestUrl = event.request.clone();
-					return fetch(requestUrl);
-				})
-				.catch((err) => console.log('err', err))
+			caches.match(event.request).then(function(response) {
+				if (response) {
+					return response; // if valid response is found in cache return it
+				} else {
+					return fetch(event.request) //fetch from internet
+						.then(function(res) {
+							return caches.open(CACHE_NAME).then(function(cache) {
+								cache.put(event.request.url, res.clone()); //save the response for future
+								return res; // return the fetched data
+							});
+						})
+						.catch(function(err) {
+							// fallback mechanism
+							return caches.open(CACHE_CONTAINING_ERROR_MESSAGES).then(function(cache) {
+								return cache.match('/offline.html');
+							});
+						});
+				}
+			})
 		);
 	}
 });

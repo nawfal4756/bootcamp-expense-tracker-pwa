f7f9a251375d28658754aacd09e8a7f94debc58e
nawfal4756 <nawfal4756@users.noreply.github.com>
2020-09-27 16:14:12 +0500

    Service Worker fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import swDev from './swDev';
-import { ThemeProvider } from '@material-ui/core';
-import { theme } from './Theme.styles';
-// import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "@material-ui/core";
+import { theme } from "./Theme.styles";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
-// serviceWorker.register();
-// swDev();
+serviceWorker.register();
